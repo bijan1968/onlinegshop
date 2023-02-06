5c1d29866516d9d9aa8c57810abf543f45cab882
Bijan Gayen <gayenbijan@gmail.com>
2023-02-07 00:39:29 +0530

    Initial commit

---
+++ onlineshope/groceryapp/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import Carousel,Category,Product,UserProfile,Cart,Booking,Feedback
+# Register your models here.
+admin.site.register(Carousel)
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(UserProfile)
+admin.site.register(Cart)
+admin.site.register(Booking)
+admin.site.register(Feedback)
